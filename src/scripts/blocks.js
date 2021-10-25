ce6881098837f20894dab9c06c0d3681e7b69c34
akuma-code <akumapl86@gmail.com>
2021-10-26 01:04:56 +0300

    sync

--- src/scripts/blocks.js
+++ src/scripts/blocks.js
@@ -5,12 +5,12 @@
 
 const blockTemplate = {
     data: function(dogdata = {}) {
-        const { bIndex, name, manager, id, summ, date } = dogdata;
+        const { bIndex, name, master, id, summ, date } = dogdata;
         return `<fieldset data-form-name='data'>
 <legend>
-<span data-block-data='name'># ${bIndex+1 || 'NEW'}) ${name ||''}</span>
+<span data-block-data='name'>#${bIndex+1 || 'NEW'}) ${name ||''}</span>
 <span>ведет:</span>
-<span data-block-data='master'>${manager || ''}</span></legend>
+<span data-block-data='master'>${master || ''}</span></legend>
 <span data-block-data='id'>${id || ''}</span>
 <span data-block-data='summ'>${summ || ''}</span><span>руб.</span>
 <span data-block-data='date'>${date || ''}</span>
@@ -19,9 +19,14 @@ const blockTemplate = {
     },
     options: function(checked = []) {
         if (!checked.length) return ``
-        let divlist = '';
-        checked.map(option => divlist += `<div class='block_options__elem'">${option}</div>`);
-        return divlist
+        let list = '';
+        const times = getDeadline(checked)
+        const max = times[0].name
+        checked.map(elem => {
+            if (elem === max) list += `<div class='block_options__elem maxdl' ">${elem}</div>`
+            else list += `<div class='block_options__elem'">${elem}</div>`
+        })
+        return list
     },
     control: function({ prov = false, correct = false, disp = false, obs = '' }) {
         return `
@@ -48,4 +53,48 @@ function getBlockTemplate(type = '') {
     return blockTemplate[type]
 }
 
-const gbt = getBlockTemplate('options')
+class BlockDog {
+    constructor(data_block = Block) {
+        this.data = data_block
+    }
+    blocksHTML() {
+        const obs = this.data.obs
+        const {
+            prov,
+            correct,
+            disp
+        } = this.data.options.status;
+        const Bcontrol = blockTemplate.control({
+            prov,
+            correct,
+            disp,
+            obs
+        })
+
+        const Bdata = blockTemplate.data(this.data)
+
+        const Boptions = blockTemplate.options(this.data.options.checked);
+        const blocks = { data: Bdata, options: Boptions, control: Bcontrol };
+        return blocks
+    };
+
+    blockDIV(type) {
+        const classes = {
+            control: `block_control`,
+            options: `block_options`,
+            data: `block_data`
+        }
+        const div = document.createElement('div');
+        const content = this.blocksHTML()
+        div.classList.add(classes[type]);
+        div.innerHTML = content[type];
+        return div
+    };
+
+    get blocks() {
+        const blocks = ['data', 'options', 'control'].map(this.blockDIV);
+        return blocks
+
+    }
+
+}
